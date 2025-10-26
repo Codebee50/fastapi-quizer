9e9b23cebc39a9ea68da050be0f7410797639368
Code bee <codebee345@outlook.com>
2025-10-25 22:02:06 -0400

    tes in prod

--- app/main.py
+++ app/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
 from app.quizer import routers as quizer
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
+
 
 load_dotenv()
 
 app = FastAPI()
+
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://beequizer.site", "http://localhost:3000", "http://localhost:5000"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 app.include_router(quizer.router)
